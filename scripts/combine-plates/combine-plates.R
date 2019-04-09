e1e5021834a1efad1a5c081f856e72993a0a0baf
MikedeKokkie <mikeopmail@hotmail.com>
2019-04-09 23:46:11 +0200

    Testing

--- scripts/combine-plates/combine-plates.R
+++ scripts/combine-plates/combine-plates.R
@@ -2,36 +2,28 @@
 install.packages("purrr")
 install.packages("dplyr")
 install.packages("tidyr")
+install.packages("readr")
 library(readr)
 library(purrr)
-library(dplyr)
 library(tidyr)
+library(dplyr)
 
 
-# Paths and Annotations         TODO: file.path()
-setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/lymphnode-sc-transcriptomics/workflows/combine-plates")
-annotations <- read.csv("annotations.tsv", sep = "\t")
-annotations <- filter(annotations, experiment == "timeseries", time == "2")
-setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Data/research-sbi-mouse-lymph-node-stromal-cell-time-series\Heidelberg_count_tables_2018_Aug/Heidelberg_count_tables_2018_Aug")
+for(filter in unique(annotations$time)) {
 
+  # Paths and Annotations
+  setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data")
+  annotations <- read.csv("annotations.tsv", sep = "\t")
+  annotations <- filter(annotations, experiment == "timeseries", time == filter)
+  
+  setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/2 - filteredcounts")
+  tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste(x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
+  cells <- tables %>% select("Plate", "CellID") %>% unique()
+  tables <- tables %>% select(-"cellnr", -"Plate") %>% spread(CellID,count)
+  tables[is.na(tables)] <- 0
+  
+  # Write results
+  setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/3 - combinedcounts")
+  write.csv(tables, file = paste('LNS_W',filter,'.coutt.csv',sep=""))
 
-### Old Way
-# tables <- list()
-# for (prefix in annotations$fileprefix) {
-#   table <- as.data.frame(read.csv(paste(prefix,'.coutt.csv',sep=""), sep="\t", header=TRUE))
-#   for (cell in colnames(table)) {
-#     if (cell != "GENEID") colnames(table)[colnames(table)==cell] <- paste(prefix,cell,sep="_")
-#   }
-#   tables[[prefix]] <- table
-# }
-# tables <- reduce(tables, full_join, by="GENEID")
-# tables[is.na(tables)] <- 0
-
-tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste(x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
-cells <- tables %>% select("Plate", "CellID") %>% unique()
-tables <- tables %>% select(-"cellnr", -"Plate") %>% spread(CellID,count)
-tables[is.na(tables)] <- 0
-
-
-# Write results
-write.csv(tables, file = "C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/lymphnode-sc-transcriptomics/workflows/combine-plates/normalized_counts.csv")
+}
