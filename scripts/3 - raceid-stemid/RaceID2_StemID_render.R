6a67036ccf3c23b659b3287f930539e1f191d2ae
MikedeKokkie <mikeopmail@hotmail.com>
2019-04-15 10:06:22 +0200

    First RaceID StemID Report

--- scripts/3 - raceid-stemid/RaceID2_StemID_render.R
+++ scripts/3 - raceid-stemid/RaceID2_StemID_render.R
@@ -2,10 +2,10 @@ install.packages("rmarkdown")
 require(rmarkdown)
 
 # set working directory
-setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics/scripts/3 - raceid-stemid")
+setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/scripts/3 - raceid-stemid")
 
 # load class definition and functions
 source("RaceID2_StemID_class.R")
 
 # run stem/raceID and render report 
-rmarkdown::render("RaceID2_StemID_sample.Rmd", "html_document", output_dir = "C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid")
+rmarkdown::render("RaceID2_StemID_sample.Rmd", "html_document", output_dir = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid")
