034971334208833e54574a9b778bd019cf555b2c
MikedeKokkie <mikeopmail@hotmail.com>
2019-04-23 20:16:42 +0200

    Psishuffle 2000

---
+++ scripts/3 - raceid-stemid/RaceID2_StemID_postanalysis.R
@@ -0,0 +1,11 @@
+install.packages("rmarkdown")
+require(rmarkdown)
+
+# set working directory
+setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/scripts/3 - raceid-stemid")
+
+# load class definition and functions
+source("RaceID2_StemID_class.R")
+
+# run stem/raceID and render report 
+rmarkdown::render("RaceID2_StemID_sample.Rmd", "html_document", output_dir = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid/AllPlates")
--- scripts/3 - raceid-stemid/RaceID2_StemID_render.R
+++ scripts/3 - raceid-stemid/RaceID2_StemID_render.R
@@ -8,7 +8,7 @@ setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/l
 source("RaceID2_StemID_class.R")
 
 # Run Initial Stem/RaceID Algorithm and Render Initial Report 
-rmarkdown::render("RaceID2_StemID_sample.Rmd", "html_document", output_dir = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid")
+rmarkdown::render("RaceID2_StemID_sample.Rmd", "html_document", output_dir = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid/AllPlates")
 
 # Run Post-Analysis on specific Genes/Clusters based on Initial Report 
-rmarkdown::render("RaceID2_StemID_postanalysis.Rmd", "html_document", output_dir = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid/")
+rmarkdown::render("RaceID2_StemID_postanalysis.Rmd", "html_document", output_dir = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data/4 - raceidstemid/AllPlates")
