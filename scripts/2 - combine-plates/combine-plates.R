b399ee99232241f1aca3e00de6fc8380c9ab69f0
MikedeKokkie <m2.de.kok@student.vu.nl>
2019-04-24 13:31:55 +0200

    Unfiltered Allcounts

--- scripts/2 - combine-plates/combine-plates.R
+++ scripts/2 - combine-plates/combine-plates.R
@@ -9,17 +9,17 @@ library(tidyr)
 library(dplyr)
 
 # Paths and Annotations      
-setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data")
+setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data")
 annotations <- read.csv("annotations.tsv", sep = "\t")
-setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data/2 - filteredcounts")
+setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data/1 - rawcounts")
 
 # Combine All Tables
 tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste(x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
-cells <- tables %>% select("Plate", "CellID") %>% unique()
+cells <- tables %>% select("Plate", "CellID") %>% distinct()
 tables <- tables %>% select(-"cellnr", -"Plate") %>% spread(CellID,count)
 tables[is.na(tables)] <- 0
 
 # Write results
-setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data/3 - combinedcounts")
-write.csv(tables, file = paste('alltables.csv',sep=""), row.names = FALSE)
-write.csv(cells, file = paste('allcellplateids.csv',sep=""), row.names = FALSE)
+setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data/3 - combinedcounts")
+write.csv(tables, file = paste('LNS_W_ALL.csv',sep=""), row.names = FALSE)
+write.csv(cells, file = paste('LNS_W_ALL_CELLIDS.csv',sep=""), row.names = FALSE)
