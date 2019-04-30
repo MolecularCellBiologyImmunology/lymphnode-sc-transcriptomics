c0676a6a14b12f49fede8d5f8738afe188eafd03
MikedeKokkie <mikeopmail@hotmail.com>
2019-04-30 10:37:14 +0200

    Choose RaceID input

--- scripts/1 - rough-filter/filter.R
+++ scripts/1 - rough-filter/filter.R
@@ -1,11 +1,9 @@
 # Note: This Script is mostly based on the instruction manual of RaceID2 and StemID.
-install.packages("dplyr")
-install.packages("readr")
+install.packages("dplyr", "readr")
 library(dplyr)
 library(readr)
 
-setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data")
-#setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics/data")
+setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data")
 annotations <- read.csv("annotations.tsv", sep = "\t", strip.white=TRUE)
 
 mintotal = 1500
@@ -17,10 +15,8 @@ maxexpr = 500
 for(file in annotations$fileprefix) {
   
   # Provided data has to be loaded
-  setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data/1 - rawcounts")
-  #setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics/data/1 - rawcounts")
   print(paste("Filtering File ", file, sep= ""))  
-  data <-read.csv(paste(file,'.coutt.csv', sep=""), sep="\t", header=TRUE)
+  data <-read.csv(paste("1 - rawcounts/",file ,'.coutt.csv', sep=""), sep="\t", header=TRUE)
   
   # The first column contains unique gene ids and has to be assigned as rownames:
   rownames(data) <-data$GENEID
@@ -32,7 +28,6 @@ for(file in annotations$fileprefix) {
   if ( ! is.numeric(mintotal) ) stop( "mintotal has to be a positive number" ) else if ( mintotal <= 0 ) stop( "mintotal has to be a positive number" )
   if ( ! is.numeric(minexpr) ) stop( "minexpr has to be a non-negative number" ) else if ( minexpr < 0 ) stop( "minexpr has to be a non-negative number" )
   if ( ! is.numeric(minnumber) ) stop( "minnumber has to be a non-negative integer number" ) else if ( round(minnumber) != minnumber | minnumber < 0 ) stop( "minnumber has to be a non-negative integer number" )
-
   
   data <- data[,apply(data,2,sum,na.rm=TRUE) >= mintotal]
   data <- data[apply(data>=minexpr,1,sum,na.rm=TRUE) >= minnumber,]
@@ -40,9 +35,5 @@ for(file in annotations$fileprefix) {
   
   # Write results
   fdata <- add_rownames(fdata, "GENEID")
-  setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data/2 - filteredcounts")
-  #setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics/data/2 - filteredcounts")
-  write_tsv(fdata, paste(file,'.coutt.csv', sep=""), append = FALSE)
-
-
+  write_tsv(fdata, paste("2 - filteredcounts/",file,'.coutt.csv', sep=""), append = FALSE)
 }
--- scripts/2 - combine-plates/combine-plates.R
+++ scripts/2 - combine-plates/combine-plates.R
@@ -11,15 +11,13 @@ library(dplyr)
 # Paths and Annotations      
 setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data")
 annotations <- read.csv("annotations.tsv", sep = "\t")
-setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data/1 - rawcounts")
 
 # Combine All Tables
-tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste(x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
+tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste('1 - rawcounts/',x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
 cells <- tables %>% select("Plate", "CellID") %>% distinct()
 tables <- tables %>% select(-"cellnr", -"Plate") %>% spread(CellID,count)
 tables[is.na(tables)] <- 0
 
 # Write results
-setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data/3 - combinedcounts")
-write.csv(tables, file = paste('LNS_W_ALL.csv',sep=""), row.names = FALSE)
-write.csv(cells, file = paste('LNS_W_ALL_CELLIDS.csv',sep=""), row.names = FALSE)
+write.csv(tables, file = '3 - combinedcounts/DUITS__ALL.csv', row.names = FALSE)
+write.csv(cells, file = '3 - combinedcounts/DUITS_CELLIDS.csv', row.names = FALSE)
