28dbf2a28bbb611f2996ded4ba6153c4957f1a11
MikedeKokkie <mikeopmail@hotmail.com>
2019-04-30 10:47:47 +0200

    Unfiltered Combined Plates

--- scripts/2 - combine-plates/combine-plates-by-week.R
+++ scripts/2 - combine-plates/combine-plates-by-week.R
@@ -9,7 +9,7 @@ library(tidyr)
 library(dplyr)
 
 # Get file prefixes for main loop
-setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data")
+setwd("D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data")
 annotations <- read.csv("annotations.tsv", sep = "\t")
 
 # Combine plates for each experiment time point
@@ -18,20 +18,17 @@ for(filter in unique(annotations$time)) {
   print(paste("Combining Plates for Week",filter,sep=" "))
   
   # Paths and Annotations      
-  setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data")
   annotations <- read.csv("annotations.tsv", sep = "\t")
   annotations <- filter(annotations, time == filter)
-  setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data/2 - filteredcounts")
-  
+
   # Combine All Tables
-  tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste(x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
+  tables <- lapply(annotations$fileprefix, function(x) {read_tsv(paste('1 - rawcounts/',x,'.coutt.csv',sep="")) %>% mutate('Plate'=x)}) %>% lapply(function(x) {gather(x, key='cellnr', value='count', -c('Plate','GENEID'))}) %>% bind_rows() %>% mutate("CellID" = paste(Plate,cellnr, sep = ".")) 
   cells <- tables %>% select("Plate", "CellID") %>% distinct()
   tables <- tables %>% select(-"cellnr", -"Plate") %>% spread(CellID,count)
   tables[is.na(tables)] <- 0
   
   # Write results
-  setwd("D:/Userdata/jj.koning/MIKE/seperatescripts v3/data//3 - combinedcounts")
-  write.csv(tables, file = paste('LNS_W',filter,'_combined.csv',sep=""), row.names = FALSE)
-  write.csv(cells, file = paste('LNS_W',filter,'_cellplateids.csv',sep=""), row.names = FALSE)
+  write.csv(tables, file = paste('3 - combinedcounts/LNS_W',filter,'_unfiltered_combined.csv',sep=""), row.names = FALSE)
+  #write.csv(cells, file = paste('3 - combinedcounts/LNS_W',filter,'_cellplateids.csv',sep=""), row.names = FALSE)
 
 }
