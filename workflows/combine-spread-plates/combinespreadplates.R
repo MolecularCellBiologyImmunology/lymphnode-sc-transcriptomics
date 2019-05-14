a242bef69b656bf939864a08223325398d83546f
MikedeKokkie <m2.de.kok@student.vu.nl>
2019-05-14 17:36:14 +0200

    CombineSpreadPlates

---
+++ workflows/combine-spread-plates/combinespreadplates.R
@@ -0,0 +1,58 @@
+# Load / install required packages
+#install.packages("readr", "dplyr", "tidyr", repos = "http://cran.us.r-project.org")
+library(readr)
+library(tidyr)
+library(dplyr)
+
+# Paths
+setwd("C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics/data - douwe final")
+#setwd("D:/Userdata/jj.koning/MIKE/Seperate Scripts/data - douwe final")
+
+# Create Mart Table
+source("https://bioconductor.org/biocLite.R")
+biocLite(pkgs=c("scran","DESeq2","biomaRt"), suppressUpdates = TRUE)
+library(biomaRt)
+mart <- useMart(biomart = "ensembl", dataset = "mmusculus_gene_ensembl")
+marttable <- biomaRt::getBM(attributes = c("ensembl_gene_id", "mgi_symbol", "chromosome_name", "description"), mart = mart)
+write_tsv(marttable, "genelist.tsv")
+
+# Annotations
+annotations <- read_tsv("annotations.tsv")
+
+# Script
+alltables <- data.frame()
+for(samplenumber in unique(annotations$time)) {
+  print(paste("*** Combining Plates for Week", samplenumber, sep=" "))
+  annotationspertime <- filter(annotations, time == samplenumber)
+  sampleplates <- data.frame()
+  for(prefix in annotationspertime$fileprefix) {
+    print(paste("Converting File ", prefix, sep= ""))  
+    data <- read_tsv(paste("1 - rawcounts/", prefix ,'/counts.tsv', sep=""))
+    data$geneid <- marttable[match(data$geneid ,marttable$ensembl_gene_id),]$mgi_symbol
+    data <- filter(data, !is.na(geneid))
+    data <- filter(data, geneid != "")
+    data <- mutate(data, "Plate" = prefix)
+    data <- mutate(data, "CellID" = paste(prefix, cellbc, sep = "."))
+    sampleplates <- rbind(sampleplates, data)
+    alltables <- rbind(alltables, data)
+  }
+  print(paste("*** Writing Combined Plates for Week", samplenumber,sep=" "))
+  sampleplates <- dplyr::select(sampleplates, -"cellbc", -"Plate")
+  sampleplates <- group_by(sampleplates, geneid, CellID)
+  sampleplates <- summarise(sampleplates, reads = sum(reads))
+  sampleplates <- spread(sampleplates, key = CellID, value = reads, fill = 0)
+  write.csv(sampleplates, file = paste('2 - combinedcounts/LNS_W', samplenumber, ".csv", sep=""), row.names = FALSE)
+}
+
+print("*** Writing CellID/Plate Lookup Table for All Cells")
+allcells <- distinct(dplyr::select(alltables, "Plate", "CellID"))
+write.csv(allcells, file = '2 - combinedcounts/CELLIDS_PLATES.csv', row.names = FALSE)
+
+print("*** Writing Count Table For all Plates Combined")
+alltables <- dplyr::select(alltables, -"cellbc", -"Plate")
+alltables <- group_by(alltables, geneid, CellID)
+alltables <- summarise(alltables, reads = sum(reads))
+spread(alltables, key = CellID, value = reads, fill = 0)
+write.csv(alltables, file = '2 - combinedcounts/LNS_ALL.csv', row.names = FALSE)
+
+print("Finished.")
--- workflows/raceid-stemid/RaceID3_StemID2_render.R
+++ workflows/raceid-stemid/RaceID3_StemID2_render.R
@@ -1,30 +1,46 @@
 # Automatically Determine Packages to download and install
-list.of.packages <- c("rmarkdown")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+if (!"rmarkdown" %in% installed.packages()) install.packages("rmarkdown", repos = "http://cran.us.r-project.org")
 
+
+# Set working directory and intput/output locations
+#workingdirectory = "C:/Users/Mike/Documents/WORK/Bioinformatics Project Internship/Scripts/seperate-scripts/lymphnode-sc-transcriptomics"
+#workingdirectory = "D:/Documents/SCHOOL/VU/2017-2018 Master Year 2/Project/Seperate Scripts/lymphnode-sc-transcriptomics/data")
+workingdirectory = "D:/Userdata/jj.koning/MIKE/Seperate Scripts/"
+scriptdirectory = paste(workingdirectory, "scripts/3 - raceid3stemid2", sep="/")
+inputdata = paste(workingdirectory, 'data - douwe/3 - combinedcounts/LNS_ALL.csv', sep='/')
+outputdirectory = paste(workingdirectory, "data - douwe/4 - raceid3stemid2/ALL", sep="/")
+
+
+
+# Set parameters for Markdown Knitting
+parameters = list(
+  
+  # Directories & Data
+  scriptdirectory = scriptdirectory,
+  inputdata = inputdata,
+  outputdirectory = outputdirectory,
+  
+  #Filtering
+  mintotal = 1500,
+  minexpr = 5,
+  minnumber = 1,
+  LBatch = NULL,
+  knn = FALSE,
+  CGenes = c("Pcna","Mki67","Malat1","Hspa1a","Jun", "Fos", "Ptprc"),
+  FGenes = NULL,
+  ccor = 0.4,
+  
+  # RaceID
+  maxclustnr = 50,
+  bootnr = 100,
+  
+  # StemID
+  RunStemID = TRUE,
+  pdishuf=2000,
+  scthr = 0.3)
 
 # Run Initial Stem/RaceID Algorithm and Render Initial Report 
-rmarkdown::render("RaceID3_StemID2_sample.Rmd", "html_document", output_dir = snakemake@params[['outputdir']], params =  list(
-  inputdata = snakemake@input[[]],
-  outputdirectory,
-  mintotal = snakemake@params[['mintotal']],
-  minexpr = snakemake@params[['minexpr']],
-  minnumber = snakemake@params[['minnumber']],
-  maxexpr = snakemake@params[['maxexpr']],
-  downsample = snakemake@params[['downsample']],
-  sfn = snakemake@params[['sfn']],
-  hkn = snakemake@params[['hkn']],
-  dsn = snakemake@params[['dsn']],
-  rseed = snakemake@params[['rseed']],
-  CGenes = snakemake@params[['CGenes']],
-  FGenes = snakemake@params[['FGenes']],
-  ccor = snakemake@params[['ccor']],
-  maxclustnr = snakemake@params[['maxclustnr']],
-  bootnr = snakemake@params[['bootnr']],
-  RunStemID = snakemake@params[['RunStemID']],
-  pdishuf= snakemake@params[['pdishuf']],
-  scthr = snakemake@params[['scthr']]))
+rmarkdown::render(paste(scriptdirectory, "RaceID3_StemID2_sample.Rmd", sep="/"), "html_document", output_dir = outputdirectory, params = parameters)
 
 # Run Post-Analysis on specific Genes/Clusters based on Initial Report 
-rmarkdown::render("RaceID3_StemID2_postanalysis.Rmd", "html_document", output_dir = outputdirectory)
+rmarkdown::render(paste(scriptdirectory, "RaceID3_StemID2_postanalysis.Rmd", sep="/"), "html_document", output_dir = outputdirectory)
