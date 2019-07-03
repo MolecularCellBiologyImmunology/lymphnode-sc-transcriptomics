cf63f388b2df9ab7c99e49fdbbf892da618d741d
Douwe Molenaar <d.molenaar@vu.nl>
2019-07-03 13:08:49 +0200

    renamed UMI parameters

--- workflows/map-barcodes/scripts/render_reports.R
+++ workflows/map-barcodes/scripts/render_reports.R
@@ -7,7 +7,7 @@ rmarkdown::render(
         cellbcfile = snakemake@input[['cellbcfile']],
         countsfile = snakemake@output[['countsfile']],
         samplename = snakemake@wildcards[['fileprefix']]
-        multiplicitycorrection = snakemake@params[['multiplicitycorrection']]
-        umimultiplicity = snakemake@params[['umimultiplicity']]
+        UMIcorrection = snakemake@params[['UMIcorrection']]
+        UMIcardinality = snakemake@params[['UMIcardinality']]
     )
 )
