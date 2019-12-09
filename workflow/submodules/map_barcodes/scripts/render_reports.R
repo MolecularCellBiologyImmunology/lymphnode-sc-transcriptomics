0eaf76640d7fed45a06bd2ee9e7c3e1aa9413e52
Kok <m.kok2@vumc.nl>
2019-12-09 15:31:28 +0100

    Finalizing Restructuring

---
+++ workflow/submodules/map_barcodes/scripts/render_reports.R
@@ -0,0 +1,11 @@
+rmarkdown::render(
+    'scripts/generate_counts.Rmd', 
+    output_file = snakemake@params[['outputfile']],
+    output_dir = snakemake@params[['outputdir']],
+    params = list(
+        featuretable = snakemake@input[['featuretable']],
+        cellbcfile = snakemake@input[['cellbcfile']],
+        countsfile = snakemake@output[['countsfile']],
+        samplename = snakemake@wildcards[['fileprefix']]
+    )
+)
